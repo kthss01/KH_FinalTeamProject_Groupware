8e67bef83b5e1999d8aec22af97976c2af5a92e5
kthss01 <kthss01@naver.com>
2021-11-01 22:51:27 +0900

    Revert "pom.xml 웹소켓 의존성 추가"
    This reverts commit fd35819073e285343954a972106c568323a3095b.

--- GroupwarePro/src/main/java/com/kh/spring/chat/controller/EchoHandler.java
+++ GroupwarePro/src/main/java/com/kh/spring/chat/controller/EchoHandler.java
@@ -3,6 +3,8 @@ package com.kh.spring.chat.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
