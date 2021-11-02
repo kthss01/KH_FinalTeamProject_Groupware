f76e8cf6baf5fa50b56cb1275e664a41b2b9701b
kthss01 <kthss01@naver.com>
2021-11-02 20:07:08 +0900

    junit을 이용한 테스트 예제 (단위, 통합 테스트)

--- GroupwarePro/src/main/java/com/kh/spring/calendar/model/dao/CalendarDao.java
+++ GroupwarePro/src/main/java/com/kh/spring/calendar/model/dao/CalendarDao.java
@@ -1,5 +1,16 @@
 package com.kh.spring.calendar.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring.member.model.vo.Member;
+
+@Repository
 public class CalendarDao {
+
+	public Member loginMember(SqlSessionTemplate sqlSession, Member m) {
+		Member m1 = sqlSession.selectOne("memberMapper.loginMember", m);
+		return m1;
+	}
 
 }
